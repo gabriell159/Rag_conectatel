ee46892c79d3108916a0b301a8c3517fa01e03b0
Gabriel Ferreira <gfo2130626@gmail.com>
2026-08-29 12:46:52 -0300

    feat: implementando mais testes, para encontrar um melhor padrao

--- src/01_pipeline_tratamento/02_analise.py
+++ src/01_pipeline_tratamento/02_analise.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parents[2]
 arquivo_tratado = BASE_DIR / "data" / "processed" / "log_chamados" / "chamados_clean.csv"
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.width', None)
 
 
 def executar_analises() -> None:
@@ -26,6 +28,32 @@ def executar_analises() -> None:
     print("ANALISE 3: Taxa de Escalonamento Humano por Canal")
     escalonamento_canal = df.groupby("canal")["encaminhado_humano"].mean() * 100
     print(escalonamento_canal.sort_values(ascending=False).round(2).astype(str) + "%")
+    print("\n")
+
+    print("ANALISE 4: Taxa de Escalonamento Humano por Subcategoria")
+    subcategoria_humano = df.groupby("subcategoria")["encaminhado_humano"].mean() * 100
+    print(subcategoria_humano.sort_values(ascending=False).round(2).astype(str) + "%")
+    print("\n")
+
+    print("ANALISE 5: Taxa de Subcategoria por cidade")
+    subcat_cidade = df.groupby("cidade")["subcategoria"].count()
+    print(subcat_cidade)
+    print("\n")
+
+    print("ANALISE 6: Taxa de problemas com plano por categoria")
+    cat_plano = df.groupby("plano_atual")["categoria"].count()
+    print(cat_plano)
+    print("\n")
+
+    print("ANALISE 7: Taxa de satisfacao por canal")
+    sat_canal = df.groupby("canal")["satisfacao_1_a_5"].value_counts()
+    print(sat_canal)
+    print("\n")
+
+    print("ANALISE 8: Taxa de satisfacao apos encaminhar para atendimento humano")
+    humano_canal = df.groupby("encaminhado_humano")["satisfacao_1_a_5"].value_counts()
+    print(humano_canal)
+
 
 
 if __name__ == "__main__":
